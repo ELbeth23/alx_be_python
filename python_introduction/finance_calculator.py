3bcf21bd7513f6b4775aeda3b82bc3d830bc740c
Bethel Teshome Debele <105913444+beth-El-D@users.noreply.github.com>
2025-11-09 21:17:05 +0300

    last change

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses:"))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses:"))
 interest_rate = 0.05
 
-saving = income - expenses
-annual_savings= saving * 12 + (saving * 12 * interest_rate)
+monthly_saving = monthly_income - monthly_expenses
+annual_savings= monthly_saving * 12 + (monthly_saving * 12 * interest_rate)
 
 print("Your annual savings including interest is:", annual_savings)
 
